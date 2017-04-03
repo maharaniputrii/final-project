507fcda37ce2c2fa62f0cbca864717849c762208
cenoki <crisenoki@hotmail.com>
2017-04-02 22:17:57 -0400

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,1 +1,31 @@
+<!-- ######################     Main Navigation   ########################## -->
+<nav>
+    <ul>
+        <?php
+        print '<li class="';
+        if ($path_parts['filename'] == "index") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="index.php">Home</a>';
+        print '</li>';
 
+        print '<li class="';
+        if ($path_parts['filename'] == "brazil") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="brazil.php">Brazil</a>';
+        print '</li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "indonesia") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="indonesia.php">Indonesia</a>';
+        print '</li>';
+        
+        ?>
+    </ul>
+</nav>

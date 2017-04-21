089c198d7b90b7189f9f401231c90d0ec77ade8b
cenoki <crisenoki@hotmail.com>
2017-04-21 12:17:17 -0400

    Update nav.php

--- nav.php
+++ nav.php
@@ -26,6 +26,29 @@
         print '<a href="indonesia.php">Indonesia</a>';
         print '</li>';
         
+        print '<li class="';
+        if ($path_parts['filename'] == "uk") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="uk.php">United Kingdom</a>';
+        print '</li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "spain") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="spain.php">Spain</a>';
+        print '</li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "form") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="form.php">Form</a>';
+        print '</li>';
         ?>
     </ul>
 </nav>

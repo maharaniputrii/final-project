7a7f999c60fa37bdea133c7f4e9b2dc6e85f4e9a
cenoki <crisenoki@hotmail.com>
2017-04-24 23:36:15 -0400

    Add files via upload

---
+++ spain.php
@@ -0,0 +1,26 @@
+<?php
+include ("top.php");
+
+?>
+
+    <article>
+    
+        <h1>Spain</h1>
+        
+        <p>Spain is located in the Iberian Peninsula and is currently part of the European Union and its government is a parliamentary monarchy.</p>
+        <p></p>
+        <p>The official language of Spain is Spanish however the autonomous communities are allowed to recognize their dominant regional languages and dialects as having official status being “co-official” languages.  Some examples are: Catalan in Catalonia and in the Balearic Islands, Valencian in Valencia, Galician (Gallego) in Galicia.</p>
+        <p>Spain is divided into 17 autonomous regions.</p>
+        <p>Catalonia is one of them. Its capital is Barcelona a very famous touristic destination. The city is mixture of Roman remains, medieval quarters and 20th century Modernism and avant-garde architecture. The constructions by the Catalan architects Antoni Gaudí and Lluís Doménech i Montaner have been declared World Heritage Sites by UNESCO. La Sagrada Familia one of Barcelona landmarks and it was designed by Gaudi. It is a Roman Catholic church that unfortunately remains unfinished since Gaudi passed away during its construction. Parc Güell, La Pedrera and Casa Batlló are other famous Gaudi works in Barcelona. The city is also home of the renowned soccer team Barcelona FC.</p>
+        <p>Community of Madrid is another autonomous region and the city of Madrid is in it. The city is the capital of Spain and is the third largest city in the European Union. It is a metropolis that combines modern infrastructures with historical centers. It is a cultural center with several museums, some of the most famous being: Museo del Prado, Museo Thyssen-Bornemisza and Museo Nacional Centro de Arte Reina Sofia. The city is also home of two world-famous soccer teams, Real Madrid CF and Atletico Madrid.</p>
+        
+        
+        
+<?php
+include ("footer.php");
+?>   
+    </article>
+
+</body>
+    
+</html>
---
+++ uk.php
@@ -0,0 +1,32 @@
+<?php
+include ("top.php");
+
+?>
+
+    <article>
+    
+        <h1>United Kingdom</h1>
+        
+        <aside class="quote">
+            <p>“Strange as it might sound I miss the public transport from the UK (which I never thought I’d say as it drives me crazy when I’m home) coming to the USA I’ve realised how amazing it actually is and how necessary it is for this country. It would be so much easier to travel!”</p>
+        </aside>
+        
+        <p>The UK is an Island nation composed by England, Wales, Scotland and Northern Ireland, the capital being London. It is a constitutional monarchy and the head of the State is Queen Elizabeth II.</p>
+        <p>The United Kingdom has made several contributions to the world. Economical,  technological and industrious.  But also cultural, with literature, films and music.</p>
+        <p>In terms of writers the UK has several incredible  famous ones. William Shakespeare, considered by some the greatest dramatist of all times was British, and since him there were plenty of internationally successful writes, such as Jane Austin, Charles Dickens, George Orwell, Arthur Conan Doyle, and many more. The last being the creator of Sherlock Homes probably the most famous fictional detective of all times, having been transformed into movies and television series. More recently J.K. Rolling with the Harry Potter novels took the world by surprise and became an incredibly  accomplished writer and the Harry Potter series a massive hit.</p>
+        <p>In the music industry the UK provided the world with some of the most successful and revolutionary musicians. The Beatles, a band formed in Liverpool England, is one of the biggest bands of all time having influenced pop culture in way no other band has. But they are definitely not alone in the British music world, The Rolling Stones, Led Zepplin,  Pink Floyd, Queen, Elton John, David Bowie are some of others influential musicians. In the more recent music scene Adele, Ed Sheeran and Coldplay are some of the famous names.</p>
+        
+        
+        
+        
+        
+        
+        
+<?php
+include ("footer.php");
+?>   
+    </article>
+
+</body>
+    
+</html>

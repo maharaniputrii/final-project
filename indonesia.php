b954ad7a978761d31f624a53d566b60a9770e2de
maharaniputrii <maharani_305@yahoo.com>
2017-04-26 00:08:54 -0400

    Update indonesia.php

--- indonesia.php
+++ indonesia.php
@@ -1,1 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>INDONESIA</title>
+        <meta charset="utf-8">
+        <meta name="author" content="CSS">
+        <meta name="description" content="Putri Rustam" >
+
+        <!-- Link to your custom style below -->
+        <link rel="stylesheet" href="custom.css" type="text/css"
+    </head>
+    <!-- ################ body section ######################### -->
+    <body id="indonesia">
+        <article class='Indonesia'>
+            <h1>INDONESIA</h1>
+            <p>Indonesia is located in the Southeast Asia. Indonesia consists of hundreds of distinct 
+                native ethnic and linguistic groups. 
+                The largest – and politically dominant – ethnic group are the Javanese.
+                Jakarta is the capital city of Indonesia.</p>
+            <figure>
+                <img src="indonesia.jpg" alt="The Map of Indonesia">
+                <figcaption>The Map of Indonesia</figcaption>
+            </figure>
+            
+            <h2>Tourism</h2>
+            <ol>
+                <li>Bali
+                    <p>Indonesia is a tropical country, which makes it attractive. There are many places that some tourists could go in Indonesia. One of the examples is Bali.
+                    Bali island has received the Best Island award from Travel and Leisure in 2010. According to BBC Travel released in 2011, Bali 
+                    is one of the World's Best Island, ranking second after Santorini, Greece. The average 
+                    temperature per-year is around 30 degrees celcius. The main location of tourism are : </p>
+                        <ol>
+                            <li>The town of Kuta (with its beach) <img src="kutabali.jpg" alt="Kuta, Bali"></li>
+                            <li>Legian and Seminyak(where some people usually go to shops. <img src="seminyak.jpg" alt = "Seminyak, Bali"></li>
+                            <li>Ubud : the center of the island.
+                                <img scr="ubud.jpeg" alt="Ubud, Bali"></li>
+                            <li>Potato Head, Bali.  </li>
+                        </ol>
+                </li>
+                <li>Raja Ampat, Papua
+                    <p>Raja Ampat that is located in Papua. Some people call it an Indonesian Island Paradise. 
+                    According to The Nature Conservancy and Conservation International, there are 75 percent of the world's species live in Raja Ampat.
+                    Most of the main activities that tourists (including divers) always do in Raja Ampat is that they will join a tour and
+                    stay in the private villa or resort during their stay. 
+                    <img src='rajaampat.jpeg' alt='Raja Ampat, Indonesia'></p>
+                   
+                </li>
+            </ol>
+
+        
+    </body>
+</html>
 

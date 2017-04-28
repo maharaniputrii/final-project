05b6ec83d4b92e2b43d4f6b3442260d3f33758d3
cenoki <crisenoki@hotmail.com>
2017-04-28 16:36:49 -0400

    Update brazil.php

--- brazil.php
+++ brazil.php
@@ -44,20 +44,34 @@ include ("top.php");
             <p>Population: (2016 est.) 207,345,000</p>
         </aside>
             
-        <p>Brazil is the biggest country in South America and it shares inland borders with every other country in South America except Chile and Ecuador. It is the fifth largest country in the world and the fifth most populous. It contains a large set of tropical and subtropical landscapes.</p>
-        <p>Brazil was colonized by Portugal and therefore the language spoken is Portuguese, however there are still some native Brazilian Indians who speak in their own dialect.</p>
-        <p>Cultural and ethnical diversity is big in Brazil. Throughout its history Brazil had different waves of immigrants, Portuguese initially and then Italian, German and from others European countries as well as Japanese during the 19th and 20th century. And it also had a huge amount of African slaves. The natives Indians after colonization had its number decreased and now account for a very small percentage of the total population. Nowadays Brazil is a mix of all these different ethnicities and cultures, intercultural marriage is common which leads to a very diverse community. In addition Brazil has absorbed and integrated parts of the cultures of all these different people into the Brazilian culture, and because it has a vast territory some regions suffered more influence from a determined group than others.</p>
-        <p>The main things Brazil is famous for are: soccer, Carnival, and the city of Rio.</p>
+        <p>Brazil is the biggest country in South America and the fifth largest and the fifth most populous country in the world. It contains a large set of tropical and subtropical landscapes.</p>
+        <p>Cultural and ethnical diversity is big in Brazil. Throughout its history Brazil had different waves of immigrants, Portuguese (the colonizers) initially and then Italian, German and from others European countries as well as Japanese during the 19th and 20th century. The natives Indians after colonization had its number decreased and now account for a very small percentage of the total population and African descendents are numerous because of the African slavery during colonization. Nowadays Brazil is a mix of all these different ethnicities and cultures, intercultural marriage is common which leads to a very diverse community. In addition Brazil has absorbed and integrated parts of the cultures of all these different people into the Brazilian culture.</p>
+        
+        <h2>Tourism</h2>
+        <p>Brazil attracts several tourists every year, it is considered an exotic place to visit.</p>
+        <h3>Rio de Janeiro</h3>
+        <p>Rio de Janeiro is the most internationally famous city of Brazil because of its touristic attractions as well as the Carnival. It has beautiful beaches, including the beach of Copacabana. It also has one of the “New Seven Wonders in the World”, the statue of “Cristo Redentor” (Christ the Redeemer). The weather is always pleasant and the people are know for being welcoming and friendly.</p>
+        <h3>Amazon rainforest</h3>
+        <p>Many tourists who want to spend time in contact with nature choose to explore the bio diversity present in the Amazon rainforest, the world’s largest rainforest. Its a very famous ecotourism location having land tours as well as river tours in the Amazon River (biggest drainage system in the world).</p>
+        <h3>Iguazu Falls</h3>
+        <p>The Iguazu Falls are the largest waterfall system in the world, a UNESCO World Heritage Site and one of the New Seven Wonders of Nature, it is located in the border of Brazil and Argentina. And it attracts many tourists for its mesmerizing views.</p>
+        <h3>Sao Paulo</h3>
+        <p>Sao Paulo is another city in Brazil know internationally but not as touristic as Rio. It is the biggest city in the country with a population of 12.04 million (2016). The city is very diverse and dynamic, whatever you want to do you are going to find it in Sao Paulo. And it is also the foremost industrial center in Latin America.</p>
+       
+        <h2>Interesting facts</h2>
+        <p>Some of the main things Brazil is famous for are: soccer, Carnival, the city of Rio and friendly people.</p>
         <p>Soccer is without a doubt the most famous and popular sport in the country. And Brazil is good at it. It has won the FIFA World Cup five times being the country with more titles, and it is also the only country to have participated in all of the editions.</p>
         <p>Brazil’s Carnival is a week of celebrations and parties  (depending on the State even longer). Historically it is a continuation of a Portuguese celebration that took place the day before Lent begins. Nowadays the Rio’s Carnival is the most famous celebration, it involves hundreds of block parades from the Samba schools, who do the parade in their allegoric cars with hundreds of people “sambando” (dancing samba).</p>
-        <p>Rio de Janeiro, is the most internationally famous city of Brazil, not only because of the Carnival but also because of its touristic attractions. It has beautiful beaches, including the beach of Copacabana. It also has one of the “New Seven Wonders in the World”, the statue of “Cristo Redentor” (Christ the Redeemer). The weather is always pleasant and the people are know for being welcoming and friendly.</p>
-        <p>Some other things Brazil people should know about Brazil.</p>
-        <p>Brazilian food is amazing. Some of the dishes are: “Feijoada”, a black bean and pork meat stew, Brazil’s most traditional dish; “Churrasco”, the Brazilian barbecue not like any other barbecue you have ever eaten; “Pao de queijo” (cheese bread)  a delicious snack eaten in breakfast or in the afternoon; “Brigadeiro”, most famous dessert made with condensed milk and chocolate.</p>
-        <p>Brazilians are know for being very friendly, and it is true that normally Brazilians are very open and welcoming. One thing outsiders might find strange is that touching other people is very common in the culture.When you first meet someone you already greet them with a hung and kiss on the cheek (depending on the region one in each cheek), and throughout the conversation people often touch each other in the arm. That personal space that is very valued on various cultures is almost non-existing in Brazil. And people also tend to be be very expressive, gesturing a lot and talking loudly.</p>
-        <p>Brazil attracts several tourists every year, it is considered an exotic place to visit. Rio is one of the main destinations, but not the only one. Many tourists who want to spend time in contact with nature choose to explore the biodiversity present in the Amazon rainforest, the world’s largest rainforest. Others decide to go to the Iguazu Falls, the largest waterfall system in the world, a UNESCO World Heritage Site and one of the New Seven Wonders of Nature.</p>
-        <p>Another important city in Brazil, also know internationally but not as touristic, is Sao Paulo. The biggest city in Brazil with a population of 12.04 million (2016). Being is very diverse and dynamic, whatever you want to do you are going to find it in Sao Paulo. And it is also the foremost industrial center in Latin America.</p>
-       
+        <p>Brazilians are know for being very friendly, and it is true that normally Brazilians are very open and welcoming. One thing outsiders might find strange is that touching other people is very common in the culture.When you first meet someone you already greet them with a hung and kiss on the cheek (depending on the region one in each cheek). That personal space that is very valued on various cultures is almost non-existing in Brazil. And people also tend to be be very expressive, gesturing a lot and talking loudly.</p>
         
+        <h2>Food</h2>
+        <p>Brazilian food is amazing! Some of the most famous dishes are:</p>
+        <p>“Feijoada”, a black bean and pork meat stew, Brazil’s most traditional dish</p>
+        <p>“Churrasco”, the Brazilian barbecue not like any other barbecue you have ever eaten, big pieces of meat are roasted with charcoal and seasoned with rock salt</p>
+        <p>“Pao de queijo” (cheese bread), a delicious snack eaten in breakfast or in the afternoon made with cassava starch and cheese</p>
+        <p>“Brigadeiro”, most famous dessert made with condensed milk and chocolate.</p>
+        <p>"Caipirinha", Brazil's traditional drink, the original one is made with lime and "cachaça" (sugar cane liquor) having variations made with other fruits and liquor</p>
+
     <?php
         include ("footer.php");
     ?>   

898f9e7cd9d7ce9bc21c4b8d8b63b71331b7101b
cenoki <crisenoki@hotmail.com>
2017-04-30 10:56:34 -0400

    Update spain.php

--- spain.php
+++ spain.php
@@ -3,24 +3,74 @@ include ("top.php");
 
 ?>
 
-    <article>
-    
-        <h1>Spain</h1>
+    <aside class="students">
+            <h1>Spanish at UVM</h1>
+            <figure class="portrait">
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <p>Name: Cristine Sudo Enoki</p>
+            <p>Age: 18 years old</p>
+            <p>Year: Freshman</p>
+            <p>Hometown: Ribeirao Preto, SP, Brazil</p>
+        </aside>
+
+        <aside>
+            <p class="quote">“One of the things I miss most about Brazil is the food. Only when I left I realized all the dishes that they don’t have here in the US. Such a shame, Americans don’t know what they’re missing out!”</p>
+            <p class="name">Cristine Enoki</p>
+        </aside>
         
-        <p>Spain is located in the Iberian Peninsula and is currently part of the European Union and its government is a parliamentary monarchy.</p>
-        <p></p>
+        <figure class="firstimg">
+            <img src="images/madrid.jpg" alt="Madrid, Spain">
+                <figcaption></figcaption>
+        </figure>
+        
+        
+        
+        
+        <article>
+            <h1>Spain</h1>
+            
+         <aside class="information">
+            <h1>Spain</h1>
+            <figure class="flag">
+                <img src="images/flagspain.png" alt="Spanish Flag">
+                <figcaption></figcaption>
+            </figure>
+            <p>Official language: Castilian Spanish</p>
+            <p>Capital: Madrid</p>
+            <p>Government: Constitutional Monarchy</p>
+            <p>Total area: 505,991 km2</p>
+            <p>Population: (2016 est.) 46,370,000</p>
+        </aside>
+        
+        <p>Spain is located in the Iberian Peninsula and is currently part of the European Union. And the country is divided into 17 autonomous regions.</p>
         <p>The official language of Spain is Spanish however the autonomous communities are allowed to recognize their dominant regional languages and dialects as having official status being “co-official” languages.  Some examples are: Catalan in Catalonia and in the Balearic Islands, Valencian in Valencia, Galician (Gallego) in Galicia.</p>
-        <p>Spain is divided into 17 autonomous regions.</p>
-        <p>Catalonia is one of them. Its capital is Barcelona a very famous touristic destination. The city is mixture of Roman remains, medieval quarters and 20th century Modernism and avant-garde architecture. The constructions by the Catalan architects Antoni Gaudí and Lluís Doménech i Montaner have been declared World Heritage Sites by UNESCO. La Sagrada Familia one of Barcelona landmarks and it was designed by Gaudi. It is a Roman Catholic church that unfortunately remains unfinished since Gaudi passed away during its construction. Parc Güell, La Pedrera and Casa Batlló are other famous Gaudi works in Barcelona. The city is also home of the renowned soccer team Barcelona FC.</p>
-        <p>Community of Madrid is another autonomous region and the city of Madrid is in it. The city is the capital of Spain and is the third largest city in the European Union. It is a metropolis that combines modern infrastructures with historical centers. It is a cultural center with several museums, some of the most famous being: Museo del Prado, Museo Thyssen-Bornemisza and Museo Nacional Centro de Arte Reina Sofia. The city is also home of two world-famous soccer teams, Real Madrid CF and Atletico Madrid.</p>
         
+        <h2>Tourism</h2>
+        <p>Spain is a very touristic location in Europe</p>
+        <h3>Barcelona</h3>
+        <p>Barcelona is the capital of Catalonia, one of the autonomous regions and it is very famous touristic destination. The city is a mixture of Roman remains, medieval quarters and 20th century Modernism and avant-garde architecture. The constructions by the Catalan architects Antoni Gaudí and Lluís Doménech i Montaner have been declared World Heritage Sites by UNESCO. La Sagrada Familia one of Barcelona landmarks and it was designed by Gaudi. It is a Roman Catholic church that unfortunately remains unfinished since Gaudi passed away during its construction. Parc Güell, La Pedrera and Casa Batlló are other famous Gaudi works in Barcelona. The city is also home of the renowned soccer team Barcelona FC.</p>
+        <h3>Madrid</h3>
+        <p>Madrid is the capital of Spain and is the third largest city in the European Union. It is a metropolis that combines modern infrastructures with historical centers. It is a cultural center with several museums, some of the most famous being: Museo del Prado, Museo Thyssen-Bornemisza and Museo Nacional Centro de Arte Reina Sofia. The city is also home of two world-famous soccer teams, Real Madrid CF and Atletico Madrid.</p>
+        <h3>Balearic islands</h3>
+        <p>Balearic islands, an autonomous region, is an archipelago of Spain in the Mediterranean Sea and a favorite holiday destination for many Europeans. Majorca, Ibiza, Minorca and Formentera are the four largest islands they have beautiful coves and natural landscapes. Ibiza in particular is know for its exciting nightlife and electronic music scene attracting many party goers. </p>
         
+        <h2>Interesting facts</h2>
+        <p>Flamenco is one of the famous things about Spain characterized by the passion performers present. It is an art-form of dance, song and instrumental (specially guitar) that is associated in the Southern region of Spain, Andalusia, and has a mysterious origin. The dancer is accompanied by the singer and guitar player, the dance is a mixture of choreographed steps and improvisation.</p>
+        <p>Bullfighting ("corrida de toros") is another tradition in Spain known internationally. It is an art form and sport connected with the country history and culture. It originated in ancient times, was  popular in Rome but it fully developed in the Iberian Peninsula</p>
         
+        <h2>Food</h2>
+        <p>Spanish cuisine is very rich! Some of the most famous dishes are:</p>
+        <p>"Tapas", which is not a specific dish but a variety of appetizers and snacks that are served in bars and restaurants.</p>
+        <p>"Paella", a Valencian rice dish, it has many variations but the original one consists of green and white beans, snails and meat of either chicken or rabbit, a famous variation is the seafood paella;</p>
+        <p>"Tortilla Española" (Spanish omelette), made with potatoes and eggs</p>
+        <p>"Jamón Ibérico" (Iberian ham), a special type of cured ham</p>
+        <p>"Gazpacho", a soup made of raw vegetables and served cold.</p>
+        <p>"Patatas bravas", fried potatoes cut in irregular shapes served with sauce, the traditional being spicy tomato sauce and aioli sauce made of garlic and olive oil.</p>
 <?php
 include ("footer.php");
 ?>   
     </article>
 
 </body>
-    
-</html>

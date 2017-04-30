1591c669df8bda1242c20af76a1aceefc757acd6
cenoki <crisenoki@hotmail.com>
2017-04-30 10:56:02 -0400

    Update uk.php

--- uk.php
+++ uk.php
@@ -2,21 +2,73 @@
 include ("top.php");
 
 ?>
+    <aside class="students">
+            <h1>British at UVM</h1>
+            <figure class="portrait">
+                <img src="" alt="">
+                <figcaption></figcaption>
+            </figure>
+            <p>Name: Kate Foster</p>
+            <p>Age: 22 years old</p>
+            <p>Year: Exchange student</p>
+            <p>Hometown: Birmingham, UK</p>
+        </aside>
 
-    <article>
-    
-        <h1>United Kingdom</h1>
-        
-        <aside class="quote">
-            <p>“Strange as it might sound I miss the public transport from the UK (which I never thought I’d say as it drives me crazy when I’m home) coming to the USA I’ve realised how amazing it actually is and how necessary it is for this country. It would be so much easier to travel!”</p>
+        <aside>
+            <p class="quote">“Strange as it might sound I miss the public transport from the UK (which I never thought I’d say as it drives me crazy when I’m home) coming to the USA I’ve realised how amazing it actually is and how necessary it is for this country. It would be so much easier to travel!”</p>
+            <p class="name">Kate Foster</p>
         </aside>
         
-        <p>The UK is an Island nation composed by England, Wales, Scotland and Northern Ireland, the capital being London. It is a constitutional monarchy and the head of the State is Queen Elizabeth II.</p>
+        <figure class="firstimg">
+            <img src="images/london.jpg" alt="London, England, UK">
+                <figcaption></figcaption>
+        </figure>
+        
+        
+        
+        
+        <article>
+            <h1>United Kingdom</h1>
+            
+         <aside class="information">
+            <h1>United Kingdom</h1>
+            <figure class="flag">
+                <img src="images/flaguk.png" alt="British Flag">
+                <figcaption></figcaption>
+            </figure>
+            <p>Official language: English; both English and Scots Gaelic in Scotland; both English and Welsh in Wales</p>
+            <p>Capital: London</p>
+            <p>Government: Constitutional Monarchy</p>
+            <p>Total area: 242,495 km2</p>
+            <p>Population: ((2016 est.) 65,630,000</p>
+        </aside>
+    
+        
+        
+        
+        <p>The UK is an Island nation composed by England, Wales, Scotland and Northern Ireland. It is a constitutional monarchy and the head of the State is Queen Elizabeth II.</p>
+        
+        <h2>Tourism</h2>
+        <h3>London</h3>
+        <p>London is the largest and most populous metropolis in the UK and the country's economic, transportation and cultural center.  It is also one of the oldest large cities in the world. It is one of the most visited cities in the globe both for business and tourism. London has several famous sightseeing spots such as the Big Bang, the clock in the Westminster Palace; the Buckingham Palace, The London home for the monarchs and the administrative head office and it is where the famous changing of the guards ceremony takes place; The London Eye a huge ferris wheel that lead to an amazing view of the city; The British Museum, museum from prehistoric to modern times; Tate Modern, museum of modern and contemporary art</p>
+        
+        <h3>Edinburgh</h3>
+        <p>Edinburgh is the capital of Scotland and its landscape is defined by rocky  hills. Some of the famous sights are Edinburgh Castle, Royal Yacht Britannia, Arthur's seat, Mary King's Close. And  the city also attract tourists for its festivals.</p>
+        
+        <h3>Manchester</h3>
+        <p>Manchester located in England is in UK's second most populous area. The city is famous for its music scene being the starting place for bands like The Smiths and Oasis and also is famous for being the home of the soccer team Manchester United F.C..Some of the famous touristic place are: Museum of Science and Industry, National Football Museum, Albert Square, The John Rylands Library.</p>
+        
+        <h2>Interesting facts</h2>
         <p>The United Kingdom has made several contributions to the world. Economical,  technological and industrious.  But also cultural, with literature, films and music.</p>
         <p>In terms of writers the UK has several incredible  famous ones. William Shakespeare, considered by some the greatest dramatist of all times was British, and since him there were plenty of internationally successful writes, such as Jane Austin, Charles Dickens, George Orwell, Arthur Conan Doyle, and many more. The last being the creator of Sherlock Homes probably the most famous fictional detective of all times, having been transformed into movies and television series. More recently J.K. Rolling with the Harry Potter novels took the world by surprise and became an incredibly  accomplished writer and the Harry Potter series a massive hit.</p>
         <p>In the music industry the UK provided the world with some of the most successful and revolutionary musicians. The Beatles, a band formed in Liverpool England, is one of the biggest bands of all time having influenced pop culture in way no other band has. But they are definitely not alone in the British music world, The Rolling Stones, Led Zepplin,  Pink Floyd, Queen, Elton John, David Bowie are some of others influential musicians. In the more recent music scene Adele, Ed Sheeran and Coldplay are some of the famous names.</p>
         
-        
+        <h2>Food</h2>
+        <p>Tea, is one of Britain's most consumed products having a dense variety of types and it is usually served with milk. Drinking tea is something entrenched in the British culture from the tea break to the afternoon tea.</p>
+        <p>Fish and Chips, the classic British dish, fried fish with French fries and malt vinegar and tatar sauce</p>
+        <p>Scone, biscuit like cake lightly sweetened with fruit added to it sometimes</p>
+        <p>Shepherd's pie, a meat pie with mashed potatoes crust</p>
+        <p>Scotch eggs, boiled eggs wrapped in sausage meat and coated in breadcrumbs, baked or deep fried</p>
         
         
         

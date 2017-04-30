850bc5266e6bc2660815bb8550f9effdd84a8e52
cenoki <crisenoki@hotmail.com>
2017-04-30 15:08:22 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include ("top.php");
+
+?>
+
+<article>
+
+    
+    <figure class="firstimg">
+        <img src="images/worldmap.jpg" alt="World Map">
+                <figcaption></figcaption>
+    </figure>
+    
+    <aside class="information">
+        <h1>This Website</h1>
+        <p>International students are a great way to learn about different countries and cultures of all over the world. Because its their culture, their country.</p>
+        <p>This website gives these students a chance to share things about their countries and about themselves.</p>
+        <p>Living abroad, in a different culture, a lot of times in a different language can be very challenging, but it is an amazing experience. And it is nice telling people things about your country, your home that they did not know and sometimes change the stereotypes they had before about your country. And also learning about different countries from other international students is priceless, its so much better learning from someone who is actually from the place, from the culture.</p>
+    </aside>
+    
+        <p>The University of Vermont accepts hundreds of students from all over the world every year!</p>
+        <p>This is a website made by two of these students for all international students.</p>
+        <p>Here you will find information about the home country of some of us as well as something about each one of us.</p>
+        <p>If you are an international student and want to be represented in this website with information about your home country, please fill out the form to receive more information about how to make this happen! We would love more contributions!</p>
+   
+        <h1>The University of Vermont</h1>
+        
+        <p>The University of Vermont,  informally known as UVM  (Universitas Viridis Montis, latin for University of the Green Mountains) is a public institution founded in 1791. It is set in a suburban area, in the city of Burlington, VT. Which is considered one of the best college towns in the country and is surrounded by mountains, lakes, rivers, forests and rolling pastures of the Vermont countryside.</p>
+        <p>UVM academic program combines faculty-student relationships characteristic of small liberal arts colleges with the resources of a major research institution.</p>
+        <p>The university is home to 10,267 undergraduates, 1,462 graduate students, 461 medical students. It offers degree programs in over 100 areas of study.</p>
+        <p>There are more than 200 student organizations and clubs on campus, including about 15 fraternities and sororities. The Catamounts athletic teams compete in the NCAA Division I America East Conference,  the mascot being a mountain cat named Rally.</p>
+        
+        <h2>International students at UVM</h2>
+        
+        <p>The University of Vermont has currently 800 international students enrolled from over 60 countries. And the university is always looking for ways to make those numbers grow.</p>
+        <p>Many international students  directly enroll in undergraduate and graduate programs. But the university also offers pathway programs which is how  other international students start their education in the United States. For undergraduates looking for a bachelor's degree the Global Gateway Program (GGP) and the US Pathway Program (USPP) are the two options.  And for students interested in earning a master's degree-the Global Gateway Program Pre-Master's Program is the pathway program available.</p>
+        <p>In addition, UVM hosts several exchange students from partner institutions and networks overseas every year, for either a semester or one academic year long programs. These students study here during the program while also completing the degree in their home universities.</p>
+        
+        
+        
+    <?php
+        include ("footer.php");
+    ?>   
+    </article>
+
+</body>
+    
+</html>

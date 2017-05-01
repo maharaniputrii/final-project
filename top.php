9ea1b908e7d9bfb00bb27f4b0167ebe0febd4d62
cenoki <crisenoki@hotmail.com>
2017-04-30 21:22:33 -0400

    Update top.php

--- top.php
+++ top.php
@@ -7,6 +7,25 @@
         <meta name="author" content="group">
         <meta name="description" content="read this: http://moz.com/learn/seo/meta-description ">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script src="js/jquery.flexslider.js"></script>
+        
+        <script type="text/javascript">
+            var flexsliderStylesLocation = "../css/flexslider.css";
+            $('<link rel="stylesheet" type="text/css" href="'+flexsliderStylesLocation+'" >').appendTo("head");
+            $(window).load(function() {
+
+                $('.flexslider').flexslider({
+                    animation: "fade",
+                    slideshowSpeed: 3000,
+                    animationSpeed: 1000
+                });
+
+            });
+        </script>
 
         <link rel="stylesheet" href="../custom.css" type="text/css" media="screen">
       

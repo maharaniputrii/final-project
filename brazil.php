f3be8bebfb1634d9e09b45d445248f2967ed54cb
cenoki <crisenoki@hotmail.com>
2017-04-30 21:21:42 -0400

    Update brazil.php

--- brazil.php
+++ brazil.php
@@ -3,6 +3,8 @@ include ("top.php");
 
 ?>
 
+
+
         <aside class="students">
             <h1>Brazilian at UVM</h1>
             <figure class="portrait">
@@ -20,12 +22,21 @@ include ("top.php");
             <p class="name">Cristine Enoki</p>
         </aside>
         
-        <figure class="firstimg">
-            <img src="images/rio.jpg" alt="Rio de Janeiro, Brazil">
-                <figcaption></figcaption>
-        </figure>
-        
-        
+
+<!-- *****Slider***** -->
+        <div class="flexslider">
+            <ul class="slides">
+                <li>
+                    <img src="images/rio-new.jpg" alt="Rio de Janeiro, Brazil">
+                </li>
+                <li>
+                    <img src="images/iguazu-new.jpg" alt="" >
+                </li>
+                <li>
+                    <img src="images/carnival-new.jpg" alt="" >
+                </li>
+            </ul>
+        </div>
         
         
         <article>
@@ -49,28 +60,81 @@ include ("top.php");
         
         <h2>Tourism</h2>
         <p>Brazil attracts several tourists every year, it is considered an exotic place to visit.</p>
+
+        <div class="tourism">
+        <figure class="medium-img left">
+            <img src="images/copacabana.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
         <h3>Rio de Janeiro</h3>
         <p>Rio de Janeiro is the most internationally famous city of Brazil because of its touristic attractions as well as the Carnival. It has beautiful beaches, including the beach of Copacabana. It also has one of the “New Seven Wonders in the World”, the statue of “Cristo Redentor” (Christ the Redeemer). The weather is always pleasant and the people are know for being welcoming and friendly.</p>
+        </div>
+        
+        <div class="tourism">
+        <figure class="medium-img right">
+            <img src="images/amazon.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
         <h3>Amazon rainforest</h3>
         <p>Many tourists who want to spend time in contact with nature choose to explore the bio diversity present in the Amazon rainforest, the world’s largest rainforest. Its a very famous ecotourism location having land tours as well as river tours in the Amazon River (biggest drainage system in the world).</p>
+        </div>
+        
+        <div>
+        <figure class="medium-img left">
+            <img src="images/iguazu-new.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
         <h3>Iguazu Falls</h3>
         <p>The Iguazu Falls are the largest waterfall system in the world, a UNESCO World Heritage Site and one of the New Seven Wonders of Nature, it is located in the border of Brazil and Argentina. And it attracts many tourists for its mesmerizing views.</p>
+        </div>
+        
+        <div>
+        <figure class="medium-img right">
+            <img src="images/saopaulo-new.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
         <h3>Sao Paulo</h3>
         <p>Sao Paulo is another city in Brazil know internationally but not as touristic as Rio. It is the biggest city in the country with a population of 12.04 million (2016). The city is very diverse and dynamic, whatever you want to do you are going to find it in Sao Paulo. And it is also the foremost industrial center in Latin America.</p>
-       
+        </div>
+        
         <h2>Interesting facts</h2>
         <p>Some of the main things Brazil is famous for are: soccer, Carnival, the city of Rio and friendly people.</p>
-        <p>Soccer is without a doubt the most famous and popular sport in the country. And Brazil is good at it. It has won the FIFA World Cup five times being the country with more titles, and it is also the only country to have participated in all of the editions.</p>
-        <p>Brazil’s Carnival is a week of celebrations and parties  (depending on the State even longer). Historically it is a continuation of a Portuguese celebration that took place the day before Lent begins. Nowadays the Rio’s Carnival is the most famous celebration, it involves hundreds of block parades from the Samba schools, who do the parade in their allegoric cars with hundreds of people “sambando” (dancing samba).</p>
-        <p>Brazilians are know for being very friendly, and it is true that normally Brazilians are very open and welcoming. One thing outsiders might find strange is that touching other people is very common in the culture.When you first meet someone you already greet them with a hung and kiss on the cheek (depending on the region one in each cheek). That personal space that is very valued on various cultures is almost non-existing in Brazil. And people also tend to be be very expressive, gesturing a lot and talking loudly.</p>
+        <ul>
+            <li><p>Soccer is without a doubt the most famous and popular sport in the country. And Brazil is good at it. It has won the FIFA World Cup five times being the country with more titles, and it is also the only country to have participated in all of the editions.</p></li>
+            <li><p>Brazil’s Carnival is a week of celebrations and parties  (depending on the State even longer). Historically it is a continuation of a Portuguese celebration that took place the day before Lent begins. Nowadays the Rio’s Carnival is the most famous celebration, it involves hundreds of block parades from the Samba schools, who do the parade in their allegoric cars with hundreds of people “sambando” (dancing samba).</p></li>
+            <li><p>Brazilians are know for being very friendly, and it is true that normally Brazilians are very open and welcoming. One thing outsiders might find strange is that touching other people is very common in the culture.When you first meet someone you already greet them with a hung and kiss on the cheek (depending on the region one in each cheek). That personal space that is very valued on various cultures is almost non-existing in Brazil. And people also tend to be be very expressive, gesturing a lot and talking loudly.</p></li>
+        </ul>
         
         <h2>Food</h2>
         <p>Brazilian food is amazing! Some of the most famous dishes are:</p>
-        <p>“Feijoada”, a black bean and pork meat stew, Brazil’s most traditional dish</p>
-        <p>“Churrasco”, the Brazilian barbecue not like any other barbecue you have ever eaten, big pieces of meat are roasted with charcoal and seasoned with rock salt</p>
-        <p>“Pao de queijo” (cheese bread), a delicious snack eaten in breakfast or in the afternoon made with cassava starch and cheese</p>
-        <p>“Brigadeiro”, most famous dessert made with condensed milk and chocolate.</p>
-        <p>"Caipirinha", Brazil's traditional drink, the original one is made with lime and "cachaça" (sugar cane liquor) having variations made with other fruits and liquor</p>
+        <figure class="small-img left">
+            <img src="images/feijoada.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
+        <figure class="small-img left">
+            <img src="images/churrasco.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
+        <figure class="small-img left">
+            <img src="images/pao-de-queijo.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
+        <figure class="small-img left">
+            <img src="images/brigadeiro.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
+        <figure class="small-img left">
+            <img src="images/caipirinha.jpg" alt="">
+        <figcaption></figcaption>
+        </figure>
+        
+        <ul>
+            <li><p>“Feijoada”, a black bean and pork meat stew, Brazil’s most traditional dish</p></li>
+            <li><p>“Churrasco”, the Brazilian barbecue not like any other barbecue you have ever eaten, big pieces of meat are roasted with charcoal and seasoned with rock salt</p></li>
+            <li><p>“Pao de queijo” (cheese bread), a delicious snack eaten in breakfast or in the afternoon made with cassava starch and cheese</p></li>
+            <li><p>“Brigadeiro”, most famous dessert made with condensed milk and chocolate.</p></li>
+            <li><p>"Caipirinha", Brazil's traditional drink, the original one is made with lime and "cachaça" (sugar cane liquor) having variations made with other fruits and liquor</p></li>
+        </ul>
 
     <?php
         include ("footer.php");

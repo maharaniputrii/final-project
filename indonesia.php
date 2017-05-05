79db0e3d41c91402497fe109b2ab5323e5bca96c
maharaniputrii <maharani_305@yahoo.com>
2017-05-05 11:55:45 -0400

    Update indonesia.php

--- indonesia.php
+++ indonesia.php
@@ -1,1 +1,107 @@
+<?php
+include ("top.php");
+?>
+        <aside class="students">
+            <h1>Indonesian at UVM</h1>
+            <figure class="portrait">
+                <img src="images/putri.jpg" alt="Putri Rustam">
+                <figcaption></figcaption>
+            </figure>
+            <p>Name: Putri Rustam</p>
+            <p>Age: 18 years old</p>
+            <p>Year: Freshman</p>
+            <p>Hometown: Jakarta, Indonesia</p>
+        </aside>
 
+        <aside>
+            <p class="quote">"Studying abroad is one of the goal of my life. I could not believe that I would say it, but I really miss all of the foods and dishes from my country. Also, I miss my city because in Jakarta, you can do whatever you want. </p>
+            <p class="name">Putri Rustam</p>
+        </aside>
+        
+        <figure class="firstimg">
+            <img src="images/jakarta.jpg" alt="Jakarta, Indonesia">
+                <figcaption></figcaption>
+        </figure>
+        
+        
+        
+        
+        <article>
+            <h1>Indonesia</h1>
+            
+         <aside class="information">
+            <h1>Indonesia</h1>
+            <figure class="flag">
+                <img src="images/indonesianflag.png" alt="Indonesian Flag">
+                <figcaption></figcaption>
+            </figure>
+            <p>Official language: Bahasa Indonesia</p>
+            <p>Capital: Jakarta</p>
+            <p>Government: Presidential Representative Democratic Republic</p>
+            <p>Total area: 1,904,569[5] km2</p>
+            <p>Population: (2015 est.) 257.6 million</p>
+        </aside>
+
+            <h1>INDONESIA</h1>
+            <p>Indonesia is located in the Southeast Asia. Indonesia consists of hundreds of distinct native ethnic and linguistic groups.  The largest – and politically dominant – ethnic group are the Javanese. Jakarta is the capital city of Indonesia.</p>
+            
+            <h2>Tourism</h2>
+            <p>Indonesia is a tropical country, the average temperature per-year is around 30 degrees celcius. There are many places that some tourists could go in Indonesia. </p>
+                     
+                <h3>Bali</h3>
+                <p>Bali is one of the world's most popular island destination to go on vacation. It has various of tropical beaches, landscape, and lush rice terraces. Here are some places that you should visit in Bali:  </p>
+                     <ul>
+                            <li>The town of Kuta</li>
+                            <li>Legian and Seminyak(where some people usually go to shops).</li>
+                            <li>Ubud : the center of the island. </li>
+                            <li>Potato Head, Bali.  </li>
+                        </ul>
+   
+      
+                <h3>Raja Ampat, Papua</h3>
+            
+                    <p>Raja Ampat that is located in Papua. Some people call it an Indonesian Island Paradise. According to The Nature Conservancy and Conservation International, there are 75 percent of the world's species live in Raja Ampat. </p>
+                    <p>Most of the main activities that tourists (including divers) always do in Raja Ampat is that they will join a tour and stay in the private villa or resort during their stay. </p>
+                    <p>It is definitely a diver's paradise and for those who love marine life!</p>
+                
+                <h3>Yogjakarta</h3>
+                <p>Yogyakarta is a city that located in Java, Indonesia. This city is known as a center of education, also it is a popular place for classical Javanese fine art and culture such as batik, music, poetry, and puppet shows.</p>
+                <p>Yogyakarta is one of the biggest historian places in Indonesia and has become an important tourist destination in Indonesia. The places that attract tourists are Boroboudur and Prambanan temples.</p>
+                
+                <h3>Jakarta</h3>
+                <p>Jakarta is the capital city of Indonesia. Some people make it as the first or last stop in Indonesia. Your visit here would not be complete without getting absorbed into the hectic and congested city! It has many hidden treasures, nightlife, and is a shopping heaven. </p>
+                <p>Jakarta is a city that never sleeps. It is, of course, different from New York City. But once you visit here, you will be wishing that you could go back here. </p>
+                
+                <h2>Interesting Facts:</h2>  
+                <ul>
+                    <li>The president of the U.S. Barack Obama attended two schools in Jakarta, Indonesia from age 6 until 11. </li>
+                    <li>Jakarta's Gelora Bung Karno Stadium is one of the largest stadium in the world. </li>
+                    <li>Rafflesia Arnoldi, the world's biggest flower, weighs up to 15 pounds and only grows in Indonesia.</li>
+                    <li>Indonesia, under the name of Dutch East Indies, was the irst team to participate in the FIFA World Cup.</li>
+                </ul>
+                
+          
+            <h2>FOOD</h2>
+            <p>Here are some dishes that traditionally from Indonesia: </p>
+                <ul>
+                    <li>Nasi Padang</li>
+                    <p>Nasi padang is a steamed rice and served with several options of pre-cooked dishes. It originated from Padang City, West Sumatra, Indonesia. People usually eat Nasi Padang with steam plain white rice and various of meats, fish, vegetables, and spicy sauces (which we call it Sambals).</p>
+                    
+                    <li>Bakmie Ayam</li>
+                    <p>Bakmie ayam is boiled in stock and topped with succulent slices of gravy-braised chicken. And of course, most people always add sambal as extra flavor to make it more spicy!</p>
+                  
+                    <li>Bakso</li>
+                    <p>Bakso is the most favorite dishes in Indonesia. It is a savory meatball noodle soup. The meatballs itself are made from chicken, beef or pork.</p>
+                    <p>Bakso has gained an international fame when the U.S. President Barack Obama remembered it as one of his favorites during a visit to Jakarta, Indonesia.</p>
+                    
+                </ul>
+          
+        </article>
+        <?php
+include ("footer.php");
+?>   
+
+</body>
+    
+</html>
+    

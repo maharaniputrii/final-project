17bc1bd9ca5f1e5758151c91eedf614e5bd89d65
cenoki <crisenoki@hotmail.com>
2017-05-05 16:05:20 -0400

    Update brazil.php

--- brazil.php
+++ brazil.php
@@ -5,35 +5,37 @@ include ("top.php");
 
 
 
-        <aside class="students">
+        <aside class="float-right no-border">
+        <div class="students">
             <h1>Brazilian at UVM</h1>
             <figure class="portrait">
-                <img src="images/cristine.jpg" alt="Cristine Enoki">
+                <img src="images/cristinenew.jpg" alt="Cristine Enoki">
                 <figcaption></figcaption>
             </figure>
             <p>Name: Cristine Sudo Enoki</p>
             <p>Age: 18 years old</p>
             <p>Year: Freshman</p>
             <p>Hometown: Ribeirao Preto, SP, Brazil</p>
-        </aside>
-
-        <aside>
-            <p class="quote">“One of the things I miss most about Brazil is the food. Only when I left I realized all the dishes that they don’t have here in the US. Such a shame, Americans don’t know what they’re missing out!”</p>
+        </div>
+        <div class="quote">
+            <p>“One of the things I miss most about Brazil is the food. Only when I left I realized all the dishes that they don’t have here in the US. Such a shame, Americans don’t know what they’re missing out!”</p>
             <p class="name">Cristine Enoki</p>
-        </aside>
+        </div>
         
+        </aside>
 
-<!-- *****Slider***** -->
+
+        
         <div class="flexslider">
             <ul class="slides">
                 <li>
-                    <img src="images/rio-new.jpg" alt="Rio de Janeiro, Brazil">
+                    <img src="images/rio.jpg" alt="Rio de Janeiro, Brazil">
                 </li>
                 <li>
-                    <img src="images/iguazu-new.jpg" alt="" >
+                    <img src="images/praia-ceara.jpg" alt="" >
                 </li>
                 <li>
-                    <img src="images/carnival-new.jpg" alt="" >
+                    <img src="images/carnival.jpg" alt="" >
                 </li>
             </ul>
         </div>
@@ -42,7 +44,7 @@ include ("top.php");
         <article>
             <h1>Brazil</h1>
             
-         <aside class="information">
+         <aside class="information float-right">
             <h1>Brazil</h1>
             <figure class="flag">
                 <img src="images/flagbrasil.png" alt="Brazilian Flag">
@@ -54,6 +56,7 @@ include ("top.php");
             <p>Total area: 8,515,767 km2</p>
             <p>Population: (2016 est.) 207,345,000</p>
         </aside>
+            <a href="form.php"></a>
             
         <p>Brazil is the biggest country in South America and the fifth largest and the fifth most populous country in the world. It contains a large set of tropical and subtropical landscapes.</p>
         <p>Cultural and ethnical diversity is big in Brazil. Throughout its history Brazil had different waves of immigrants, Portuguese (the colonizers) initially and then Italian, German and from others European countries as well as Japanese during the 19th and 20th century. The natives Indians after colonization had its number decreased and now account for a very small percentage of the total population and African descendents are numerous because of the African slavery during colonization. Nowadays Brazil is a mix of all these different ethnicities and cultures, intercultural marriage is common which leads to a very diverse community. In addition Brazil has absorbed and integrated parts of the cultures of all these different people into the Brazilian culture.</p>
@@ -61,41 +64,42 @@ include ("top.php");
         <h2>Tourism</h2>
         <p>Brazil attracts several tourists every year, it is considered an exotic place to visit.</p>
 
-        <div class="tourism">
-        <figure class="medium-img left">
+        <section class="tourism-left">
+        <figure class="medium-img">
             <img src="images/copacabana.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>Copacabana, Rio de Janeiro</figcaption>
         </figure>
         <h3>Rio de Janeiro</h3>
         <p>Rio de Janeiro is the most internationally famous city of Brazil because of its touristic attractions as well as the Carnival. It has beautiful beaches, including the beach of Copacabana. It also has one of the “New Seven Wonders in the World”, the statue of “Cristo Redentor” (Christ the Redeemer). The weather is always pleasant and the people are know for being welcoming and friendly.</p>
-        </div>
-        
-        <div class="tourism">
-        <figure class="medium-img right">
+        </section>
+
+        <section class="tourism-right">
+        <figure class="medium-img">
             <img src="images/amazon.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>Amazon River</figcaption>
         </figure>
         <h3>Amazon rainforest</h3>
         <p>Many tourists who want to spend time in contact with nature choose to explore the bio diversity present in the Amazon rainforest, the world’s largest rainforest. Its a very famous ecotourism location having land tours as well as river tours in the Amazon River (biggest drainage system in the world).</p>
-        </div>
+        </section>
+
         
-        <div>
-        <figure class="medium-img left">
-            <img src="images/iguazu-new.jpg" alt="">
-        <figcaption></figcaption>
+        <section class="tourism-left">
+        <figure class="medium-img">
+            <img src="images/iguazu.jpg" alt="">
+        <figcaption>Iguazu Falls</figcaption>
         </figure>
         <h3>Iguazu Falls</h3>
         <p>The Iguazu Falls are the largest waterfall system in the world, a UNESCO World Heritage Site and one of the New Seven Wonders of Nature, it is located in the border of Brazil and Argentina. And it attracts many tourists for its mesmerizing views.</p>
-        </div>
+        </section>
         
-        <div>
-        <figure class="medium-img right">
+        <section class="tourism-right">
+        <figure class="medium-img">
             <img src="images/saopaulo-new.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>Octávio Frias de Oliveira Bridge, São Paulo</figcaption>
         </figure>
         <h3>Sao Paulo</h3>
         <p>Sao Paulo is another city in Brazil know internationally but not as touristic as Rio. It is the biggest city in the country with a population of 12.04 million (2016). The city is very diverse and dynamic, whatever you want to do you are going to find it in Sao Paulo. And it is also the foremost industrial center in Latin America.</p>
-        </div>
+        </section>
         
         <h2>Interesting facts</h2>
         <p>Some of the main things Brazil is famous for are: soccer, Carnival, the city of Rio and friendly people.</p>
@@ -107,26 +111,28 @@ include ("top.php");
         
         <h2>Food</h2>
         <p>Brazilian food is amazing! Some of the most famous dishes are:</p>
+        <div class="food-img">
         <figure class="small-img left">
             <img src="images/feijoada.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>"Feijoada"</figcaption>
         </figure>
         <figure class="small-img left">
             <img src="images/churrasco.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>"Churrasco"</figcaption>
         </figure>
         <figure class="small-img left">
             <img src="images/pao-de-queijo.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>"Pão de queijo"</figcaption>
         </figure>
         <figure class="small-img left">
             <img src="images/brigadeiro.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>"Brigadeiro"</figcaption>
         </figure>
         <figure class="small-img left">
             <img src="images/caipirinha.jpg" alt="">
-        <figcaption></figcaption>
+        <figcaption>"Caipirinha"</figcaption>
         </figure>
+        </div>
         
         <ul>
             <li><p>“Feijoada”, a black bean and pork meat stew, Brazil’s most traditional dish</p></li>

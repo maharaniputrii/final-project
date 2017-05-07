bab5a1dd9e636ed49822bd6ab34abf9b29bcd024
cenoki <crisenoki@hotmail.com>
2017-05-07 19:03:41 -0400

    Add files via upload

--- top.php
+++ top.php
@@ -7,7 +7,7 @@
         <meta name="author" content="group">
         <meta name="description" content="read this: http://moz.com/learn/seo/meta-description ">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
@@ -26,33 +26,21 @@
 
             });
         </script>
-
-        <link rel="stylesheet" href="../custom.css" type="text/css" media="screen">
       
-        <?php
+        <link rel="stylesheet" href="../css/custom-finalproject.css" type="text/css" media="screen">
+        
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|PT+Sans|PT+Serif" rel="stylesheet">
+        
+ <?php
         $debug = false;
-
-
-
         if (isset($_GET["debug"])) {
             $debug = true;
         }
-
-
-
-
-
-
         $domain = "//";
-
         $server = htmlentities($_SERVER['SERVER_NAME'], ENT_QUOTES, "UTF-8");
-
         $domain .= $server; 
-
         $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
-
         $path_parts = pathinfo($phpSelf);
-
         if ($debug) {
             print"<p>Domain: " . $domain;
             print'<p>php Self: ' . $phpSelf;
@@ -60,21 +48,8 @@
             print_r($path_parts);
             print'</pre></p>';
         }
-
-
-
-
-
-
-
-
         print "\n" . '<!-- include libraries -->' . "\n";
-
         require_once('lib/security.php');
-
-
-
-
         if ($path_parts['filename'] == "form"){
             print"\n<!-- include form libraries -->\n";
             include "lib/validation-functions.php";
@@ -89,9 +64,9 @@
 
     <?php
     print '<body id="' . $path_parts['filename'] . '">';
-
+    
     include 'header.php';
-    include 'nav.php';
+
     
     if ($debug) {
         print'<p> DEBUG MODE IS ON</p';

edeac0be46c7023fcb5c3acec6a7987ee44708b2
cenoki <crisenoki@hotmail.com>
2017-05-07 19:12:13 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 <!-- ######################     Page header   ############################## -->
+
 <header>
-    <h1>Diversity in University of Vermont</h1>
+        <?php include 'nav.php'; ?>
+ 
     <figure class="logo">
-        <img src="../logo.gif" alt="logo">
+        <img src="images/logo.gif" alt="logo">
     </figure>
+    <h1>Diversity in University of Vermont</h1>
 </header>
+
